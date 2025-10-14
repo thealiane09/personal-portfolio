280790684ede2d5ede9ac6e6dfb100af4841b641
Yummii09 <inalealtia15@gmail.com>
2025-10-14 19:02:56 +0800

    Done layout in App.css and App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,245 +1,146 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-        <>
+    <>
       {/* Header */}
       <div id="header">
         <div className="top">
           {/* Logo */}
           <div id="logo">
             <span className="image avatar48">
-              <img src="images/avatar.jpg" alt="" />
+              <img src="images/Thea Prof.jpg" alt="Profile" />
             </span>
-            <h1 id="title">Jane Doe</h1>
-            <p>Hyperspace Engineer</p>
+            <h1 id="title">ALTIA G. INALE</h1>
+            <p>Business Analyst</p>
           </div>
+
           {/* Nav */}
           <nav id="nav">
             <ul>
-              <li>
-                <a href="#top" id="top-link">
-                  <span className="icon solid fa-home">Intro</span>
-                </a>
-              </li>
-              <li>
-                <a href="#portfolio" id="portfolio-link">
-                  <span className="icon solid fa-th">Portfolio</span>
-                </a>
-              </li>
-              <li>
-                <a href="#about" id="about-link">
-                  <span className="icon solid fa-user">About Me</span>
-                </a>
-              </li>
-              <li>
-                <a href="#contact" id="contact-link">
-                  <span className="icon solid fa-envelope">Contact</span>
-                </a>
-              </li>
+              <li><a href="#top" id="top-link"><span className="icon solid fa-home">Biography</span></a></li>
+              <li><a href="#portfolio" id="portfolio-link"><span className="icon solid fa-th">Projects</span></a></li>
+              <li><a href="#about" id="about-link"><span className="icon solid fa-user">Skills</span></a></li>
+              <li><a href="#contact" id="contact-link"><span className="icon solid fa-user">Contact</span></a></li>
+              <li><a href="#resume" id="resume-link"><span className="icon solid fa-envelope">Resume</span></a></li>
             </ul>
           </nav>
         </div>
-        <div className="bottom">
-          {/* Social Icons */}
-          <ul className="icons">
-            <li>
-              <a href="#" className="icon brands fa-twitter">
-                <span className="label">Twitter</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands fa-facebook-f">
-                <span className="label">Facebook</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands fa-github">
-                <span className="label">Github</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon brands fa-dribbble">
-                <span className="label">Dribbble</span>
-              </a>
-            </li>
-            <li>
-              <a href="#" className="icon solid fa-envelope">
-                <span className="label">Email</span>
-              </a>
-            </li>
-          </ul>
-        </div>
       </div>
+
       {/* Main */}
       <div id="main">
-        {/* Intro */}
+        {/* Biography Section */}
         <section id="top" className="one dark cover">
           <div className="container">
             <header>
-              <h2 className="alt">
-                Hi! I'm <strong>Prologue</strong>, a{" "}
-                <a href="http://html5up.net/license">free</a> responsive
-                <br />
-                site template designed by <a href="http://html5up.net">HTML5 UP</a>.
-              </h2>
-              <p>
-                Ligula scelerisque justo sem accumsan diam quis
-                <br />
-                vitae natoque dictum sollicitudin elementum.
-              </p>
+              <h2 className="alt">Hi! I'm <strong>Altia Inale</strong></h2>
+              <p>Welcome to my personal portfolio.</p>
             </header>
             <footer>
-              <a href="#portfolio" className="button scrolly">
-                Magna Aliquam
-              </a>
+              <a href="#portfolio" className="button scrolly">View Projects</a>
             </footer>
           </div>
         </section>
-        {/* Portfolio */}
+
+        {/* Portfolio Section */}
         <section id="portfolio" className="two">
           <div className="container">
-            <header>
-              <h2>Portfolio</h2>
-            </header>
-            <p>
-              Vitae natoque dictum etiam semper magnis enim feugiat convallis
-              convallis egestas rhoncus ridiculus in quis risus amet curabitur
-              tempor orci penatibus. Tellus erat mauris ipsum fermentum etiam
-              vivamus eget. Nunc nibh morbi quis fusce hendrerit lacus ridiculus.
-            </p>
+            <header><h2>Projects</h2></header>
+            <p>Showcasing some of my works and accomplishments.</p>
             <div className="row">
               <div className="col-4 col-12-mobile">
                 <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic02.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Ipsum Feugiat</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic03.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Rhoncus Semper</h3>
-                  </header>
+                  <a href="#" className="image fit"><img src="images/pic02.jpg" alt="" /></a>
+                  <header><h3>Ipsum Feugiat</h3></header>
                 </article>
               </div>
               <div className="col-4 col-12-mobile">
                 <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic04.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Magna Nullam</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic05.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Natoque Vitae</h3>
-                  </header>
+                  <a href="#" className="image fit"><img src="images/pic04.jpg" alt="" /></a>
+                  <header><h3>Magna Nullam</h3></header>
                 </article>
               </div>
               <div className="col-4 col-12-mobile">
                 <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic06.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Dolor Penatibus</h3>
-                  </header>
-                </article>
-                <article className="item">
-                  <a href="#" className="image fit">
-                    <img src="images/pic07.jpg" alt="" />
-                  </a>
-                  <header>
-                    <h3>Orci Convallis</h3>
-                  </header>
+                  <a href="#" className="image fit"><img src="images/pic06.jpg" alt="" /></a>
+                  <header><h3>Dolor Penatibus</h3></header>
                 </article>
               </div>
             </div>
           </div>
         </section>
-        {/* About Me */}
+
+        {/* Skills Section */}
         <section id="about" className="three">
           <div className="container">
-            <header>
-              <h2>About Me</h2>
-            </header>
-            <a href="#" className="image featured">
-              <img src="images/pic08.jpg" alt="" />
-            </a>
+            <header><h2>Skills</h2></header>
+            <a href="#" className="image featured"><img src="images/pic08.jpg" alt="Skills" /></a>
             <p>
-              Tincidunt eu elit diam magnis pretium accumsan etiam id urna.
-              Ridiculus ultricies curae quis et rhoncus velit. Lobortis elementum
-              aliquet nec vitae laoreet eget cubilia quam non etiam odio tincidunt
-              montes. Elementum sem parturient nulla quam placerat viverra mauris
-              non cum elit tempus ullamcorper dolor. Libero rutrum ut lacinia donec
-              curae mus vel quisque sociis nec ornare iaculis.
+              I specialize in data analysis, system documentation, and project coordination. 
+              Passionate about improving business processes through technology and collaboration.
             </p>
           </div>
         </section>
-        {/* Contact */}
+
+        {/* Contact Section */}
         <section id="contact" className="four">
           <div className="container">
-            <header>
-              <h2>Contact</h2>
-            </header>
-            <p>
-              Elementum sem parturient nulla quam placerat viverra mauris non cum
-              elit tempus ullamcorper dolor. Libero rutrum ut lacinia donec curae
-              mus. Eleifend id porttitor ac ultricies lobortis sem nunc orci
-              ridiculus faucibus a consectetur. Porttitor curae mauris urna mi
-              dolor.
-            </p>
-            <form method="post" action="#">
+            <header><h2>Contact</h2></header>
+            <form>
               <div className="row">
                 <div className="col-6 col-12-mobile">
                   <input type="text" name="name" placeholder="Name" />
                 </div>
                 <div className="col-6 col-12-mobile">
-                  <input type="text" name="email" placeholder="Email" />
+                  <input type="email" name="email" placeholder="Email" />
                 </div>
                 <div className="col-12">
-                  <textarea
-                    name="message"
-                    placeholder="Message"
-                    defaultValue={""}
-                  />
+                  <textarea name="message" placeholder="Message"></textarea>
                 </div>
                 <div className="col-12">
-                  <input type="submit" defaultValue="Send Message" />
+                  <input type="submit" value="Send Message" />
                 </div>
               </div>
             </form>
           </div>
         </section>
+
+        {/* Resume Section */}
+        <section id="resume" className="four">
+          <div className="container">
+            <header><h2>Resume</h2></header>
+            <a
+              href="https://drive.google.com/drive/folders/1AJpkb3YfdlVvIlf775_DKeWWSlRzhVmO"
+              className="button"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              View My Resume
+              <img src="images/pic08.jpg" alt="Resume" />
+            </a>
+            <p>
+              I am Jocelyn Bendoy, 24 years old from Cabuyao, Laguna. 
+              Currently taking Associate in Computer Technology (ACT2) at La Verdad Christian College, 
+              with a ladderized path toward a BS in Information Systems. My goal is to become a competent 
+              Business Analyst who bridges the gap between technology and business needs.
+            </p>
+          </div>
+        </section>
       </div>
+
       {/* Footer */}
       <div id="footer">
-        {/* Copyright */}
         <ul className="copyright">
-          <li>© Untitled. All rights reserved.</li>
-          <li>
-            Design: <a href="http://html5up.net">HTML5 UP</a>
-          </li>
+          <li>© Altia Inale. All rights reserved.</li>
+          <li>Design by <a href="http://html5up.net">HTML5 UP</a></li>
         </ul>
       </div>
-      {/* Scripts */}
     </>
-
-  )
+  );
 }
 
-export default App
+export default App;
